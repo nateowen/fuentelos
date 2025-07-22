9ac115db123b89e07e3dfbb15e0f26b20ca8d551
Nathan Owen <nathanrheadowen@gmail.com>
2025-07-22 09:02:22 -0700

    SEO optimizations

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,9 +1,9 @@
 import { MetadataRoute } from 'next'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export async function GET(): Promise<Response> {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://fuentelos.com'
   
-  return [
+  const sitemap: MetadataRoute.Sitemap = [
     {
       url: baseUrl,
       lastModified: new Date(),
@@ -29,4 +29,22 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
   ]
-}
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${sitemap.map((item) => `
+    <url>
+        <loc>${item.url}</loc>
+        <lastmod>${item.lastModified ? (typeof item.lastModified === 'string' ? item.lastModified : item.lastModified.toISOString()) : new Date().toISOString()}</lastmod>
+        <changefreq>${item.changeFrequency}</changefreq>
+        <priority>${item.priority}</priority>
+    </url>
+    `).join('')}
+    </urlset>`
+
+    return new Response(xml, {
+        headers: {
+        'Content-Type': 'application/xml',
+        },
+    })
+    }
